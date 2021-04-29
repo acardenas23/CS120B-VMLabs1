86f9fcb9781db8aef90a5fd4a713a9b2b7cfcc97
Ubuntu <acard079@ucr.edu>
2021-04-29 15:13:18 -0700

    Initializing repo

---
+++ lab5/source/main.c
@@ -0,0 +1,144 @@
+/*	Author: Ana Cardenas Beltran
+ *  Partner(s) Name: 
+ *	Lab Section:21
+ *	Assignment: Lab #5  Exercise #1
+ *	Exercise Description: [optional - include for your own benefit]
+ *		PA3..PA0 set to value from 0 to 15
+ *		if level is 1 or 2-> PC5 lights.....
+ *	I acknowledge all content contained herein, excluding template or example
+ *	code, is my own original work.
+ */
+#include <avr/io.h>
+#ifdef _SIMULATE_
+#include "simAVRHeader.h"
+#endif
+
+enum states {START, WAIT, WAIT2, HOLDINC, HOLDDEC, INC, DEC, RESET} state;
+
+
+void TickFct()
+{
+	switch(state){
+		case START:
+			state = WAIT;
+			break;
+		case WAIT:
+			state = WAIT2;
+			break;
+		case WAIT2:
+			if((~PINA & 0x03) == 0x01){
+				state = INC;
+			}else if((~PINA & 0x03) == 0x02){
+				state = DEC;
+			}else if((~PINA & 0x03) == 0x03){
+				state = RESET;
+			}else if((~PINA & 0x03) == 0x00){
+				state =  WAIT2;
+			}
+			break;
+		case HOLDINC:
+			if((~PINA & 0x03) == 0x01){
+				state = HOLDINC;
+			}else if((~PINA & 0x03) == 0x03){
+				state = RESET;
+			}else if((~PINA & 0x03) == 0x02){
+				state = DEC;
+			}else{
+				state = WAIT2;
+			}
+			break;
+		case HOLDDEC:
+			if((~PINA & 0x03) == 0x02){
+				state = HOLDDEC;
+			}else if((~PINA & 0x03) == 0x03){
+				state = RESET;
+			}else if((~PINA & 0x03) == 0x01){
+				state = INC;
+			}else{
+				state = WAIT2;
+			}
+			break;
+
+		case INC:
+			if((~PINA & 0x03) == 0x02){
+				state = DEC;
+			}else if((~PINA & 0x03) == 0x03){
+				state = RESET;
+			}else if((~PINA & 0x03) == 0x01){
+				state = HOLDINC;
+			}else{
+				state = WAIT2;
+			}
+			
+			break;
+		case DEC:
+			if((~PINA & 0x03) == 0x01){
+				state = INC;
+			}else if((~PINA & 0x03) == 0x03){
+				state = RESET;
+			}else if((~PINA & 0x03) == 0x02){
+				state = HOLDDEC;
+			}else{
+				state = WAIT2;
+			}
+			break;
+		case RESET:
+			if((~PINA & 0x03) == 0x03){
+				state = RESET;
+			}else{
+				state = WAIT2;
+			}
+			break;
+		default:
+			state = START;
+			break;
+	}
+	switch(state){
+		case START:
+			PORTC = 0x07;
+			break;
+		case WAIT:
+			PORTC = 0x07;
+			break;
+		case WAIT2:
+			break;
+		case HOLDINC:
+			break;
+		case HOLDDEC:
+			break;
+
+		case INC:
+			if(PINC < 0x09){
+				PORTC = PINC + 0x01;
+			}else{
+				PORTC = 0x09;
+			}
+			break;
+		case DEC:
+			if(PINC > 0x00){
+				PORTC = PINC - 0x01;
+			}else{
+				PORTC = 0x00;
+			}
+			break;
+		case RESET:
+			PORTC = 0x00;
+			break;
+		default:
+			PORTC = 0x07;
+			break;
+	}
+}
+
+int main(void) {
+    /* Insert DDR and PORT initializations */
+	DDRA = 0x00; PORTA = 0xFF; // PORTA is input
+	DDRC = 0xFF; PORTC = 0x00; //PORTB is output
+	state = START;
+    /* Insert your solution below */
+    while (1) {
+	    TickFct();
+    }
+    return 1;
+}
+
---
+++ lab5/turnin/acard079_lab5_part3.c
@@ -0,0 +1,51 @@
+/*	Author: Ana Cardenas Beltran
+ *  Partner(s) Name: 
+ *	Lab Section:21
+ *	Assignment: Lab #5  Exercise #1
+ *	Exercise Description: [optional - include for your own benefit]
+ *		PA3..PA0 set to value from 0 to 15
+ *		if level is 1 or 2-> PC5 lights.....
+ *	I acknowledge all content contained herein, excluding template or example
+ *	code, is my own original work.
+ */
+#include <avr/io.h>
+#ifdef _SIMULATE_
+#include "simAVRHeader.h"
+#endif
+
+int main(void) {
+    /* Insert DDR and PORT initializations */
+	DDRA = 0x00; PORTA =  0xFF; //set PORTA as input
+	DDRC = 0xFF; PORTC = 0x00; //set PORTC as output
+
+	unsigned char tmpA = 0x00;
+	unsigned char tmpC = 0x00;
+
+	/* Insert your solution below */
+    while (1) {
+	    tmpA = (~PINA) & 0x0F;
+	    tmpC = 0x00;
+	  	    
+		//13-15 lights PC5-PC0 
+	    if(tmpA >= 0x0D){
+		    tmpC = 0x3F;
+	    }else if (tmpA >= 0x0A){
+		    tmpC = 0x3E;
+	    }else if (tmpA >= 0x07){
+		    tmpC = 0x3C;
+	    }else if (tmpA >= 0x05){
+		    tmpC = 0x38;
+	    }else if (tmpA >= 0x03){
+		    tmpC = 0x70; //0111 0000
+	    }else if (tmpA >= 0x01){
+		    tmpC = 0x60; // 0110 0000
+	    }else{
+		    tmpC = 0x40;
+	    }
+
+	    PORTC = tmpC;
+	   	    
+
+    }
+    return 1;
+}
